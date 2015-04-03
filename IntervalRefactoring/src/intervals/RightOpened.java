962a08796710e41521bf1c5c0d1670a9d1d8ea66
molinar <jmolinaromero86@gmail.com>
2015-04-03 12:19:31 +0200

    - Implement subclass RightOpened with the overrinding method for the type code

--- IntervalRefactoring/src/intervals/RightOpened.java
+++ IntervalRefactoring/src/intervals/RightOpened.java
@@ -1,5 +1,14 @@
 package intervals;
 
-public class RightOpened {
+public class RightOpened extends Interval{
+
+    public RightOpened(double minimun, double maximum) {
+        super(minimun, maximum);
+    }
+
+    @Override
+    public Opening getOpening() {
+        return Opening.RIGHT_OPENED;
+    }
 
 }
