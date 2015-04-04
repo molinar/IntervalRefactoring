a366b48fb023b09864c33fd829de4808379d84f1
molinar <jmolinaromero86@gmail.com>
2015-04-04 10:02:44 +0200

    - Add override methods in subclasses OpenPoint and ClosePoint

--- IntervalRefactoring/src/intervals/OpenPoint.java
+++ IntervalRefactoring/src/intervals/OpenPoint.java
@@ -1,5 +1,81 @@
 package intervals;
 
-public class OpenPoint {
+public class OpenPoint extends Point{
+
+    public OpenPoint(double value) {
+        super(value);
+    }
+
+    @Override
+    public boolean isLowerThan(Point point) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isLowerThan(ClosePoint point) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isLowerThan(OpenPoint point) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isGreaterThan(Point point) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isGreaterThan(ClosePoint point) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isGreaterThan(OpenPoint point) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isLowerOrEqualsThan(Point point) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isLowerOrEqualsThan(ClosePoint point) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isLowerOrEqualsThan(OpenPoint point) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isGreaterOrEqualsThan(Point point) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isGreaterOrEqualsThan(ClosePoint point) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isGreaterOrEqualsThan(OpenPoint point) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
 }
