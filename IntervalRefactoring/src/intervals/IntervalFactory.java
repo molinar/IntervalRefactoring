453e3064d7c08d17e4573397e2ad3faac79b49bf
molinar <jmolinaromero86@gmail.com>
2015-04-04 13:17:11 +0200

    - Alter IntervalFactory to create each type of interval

--- IntervalRefactoring/src/intervals/IntervalFactory.java
+++ IntervalRefactoring/src/intervals/IntervalFactory.java
@@ -6,13 +6,13 @@ public class IntervalFactory {
 	public static Interval getInterval(double minimum, double maximum, Opening opening) {
 	    switch (opening){
 	    case BOTH_OPENED:
-	        return new BothOpened(minimum, maximum);
+	        return new Interval(new OpenPoint(minimum), new OpenPoint(maximum));
 	    case LEFT_OPENED:
-	        return new LeftOpened(minimum, maximum);
+	        return new Interval(new OpenPoint(minimum), new ClosePoint(maximum));
 	    case RIGHT_OPENED:
-	        return new RightOpened(minimum, maximum);
+	        return new Interval(new ClosePoint(minimum), new OpenPoint(maximum));
 	    case UNOPENED:
-	        return new Unopened(minimum, maximum);
+	        return new Interval(new ClosePoint(minimum), new ClosePoint(maximum));
         default:
             throw new IllegalArgumentException("Incorrect type code value");
 	    }
