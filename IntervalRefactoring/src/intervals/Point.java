77732ed0e58e9b8490288722ce5ae655135bf31e
molinar <jmolinaromero86@gmail.com>
2015-04-04 10:01:02 +0200

    - Add abstract methods in class Point

--- IntervalRefactoring/src/intervals/Point.java
+++ IntervalRefactoring/src/intervals/Point.java
@@ -7,4 +7,28 @@ public abstract class Point {
     public Point(double value){
         this.value = value;
     }
+    
+    public abstract boolean isLowerThan(Point point);
+    
+    public abstract boolean isLowerThan(ClosePoint point);
+    
+    public abstract boolean isLowerThan(OpenPoint point);
+    
+    public abstract boolean isGreaterThan(Point point);
+    
+    public abstract boolean isGreaterThan(ClosePoint point);
+    
+    public abstract boolean isGreaterThan(OpenPoint point);
+    
+    public abstract boolean isLowerOrEqualsThan(Point point);
+    
+    public abstract boolean isLowerOrEqualsThan(ClosePoint point);
+    
+    public abstract boolean isLowerOrEqualsThan(OpenPoint point);
+    
+    public abstract boolean isGreaterOrEqualsThan(Point point);
+    
+    public abstract boolean isGreaterOrEqualsThan(ClosePoint point);
+    
+    public abstract boolean isGreaterOrEqualsThan(OpenPoint point);
 }
