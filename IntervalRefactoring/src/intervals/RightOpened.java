e384f3e791ea1d2b995f34d5c7bc6b0e32ae6f2b
molinar <jmolinaromero86@gmail.com>
2015-04-04 10:53:07 +0200

    - Refactoring class RightOpened with Point

--- IntervalRefactoring/src/intervals/RightOpened.java
+++ IntervalRefactoring/src/intervals/RightOpened.java
@@ -3,7 +3,9 @@ package intervals;
 public class RightOpened extends Interval{
 
     public RightOpened(double minimum, double maximum) {
-        super(minimum, maximum);
+        super();
+        this.minimum = new OpenPoint(minimum);
+        this.maximum = new OpenPoint(maximum);
     }
 
     @Override
@@ -13,31 +15,11 @@ public class RightOpened extends Interval{
 
     @Override
     public boolean includes(double value) {
-        return (value >= this.minimum && value < this.maximum);
+        return (value >= this.minimum.value && value < this.maximum.value);
     }
 
     @Override
     public boolean includes(Interval interval) {
-        return interval.includes(this);
-    }
-
-    @Override
-    public boolean includes(BothOpened interval) {
-        return (interval.minimum < this.minimum && interval.maximum >= this.maximum);
-    }
-
-    @Override
-    public boolean includes(LeftOpened interval) {
-        return (interval.minimum < this.minimum && interval.maximum >= this.maximum);
-    }
-
-    @Override
-    public boolean includes(RightOpened interval) {
-        return (interval.minimum <= this.minimum && interval.maximum >= this.maximum);
-    }
-
-    @Override
-    public boolean includes(Unopened interval) {
-        return (interval.minimum <= this.minimum && interval.maximum >= this.maximum);
+        return (this.minimum.isGreaterOrEqualsThan(interval.minimum) && this.maximum.isLowerOrEqualsThan(interval.maximum));
     }
 }
